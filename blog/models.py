341f0d96b647de4e6ee460acbf1800d24d312b33
xpsylon <xxxpsylon@gmail.com>
2023-03-18 17:15:07 +0100

    last check

--- blog/models.py
+++ blog/models.py
@@ -10,7 +10,11 @@ class Post(models.Model): #post will be the table of a database.
     content = models.TextField() #text field is text with no restrictions
     date_posted = models.DateTimeField(default=timezone.now) #we dont put timezone.now(), just timezone.now because we dont
     #want to execute the now function, but just to set it as a default value.
-    author = models.ForeignKey(User, on_delete=models.CASCADE) #ForeignKey is the ID of each table how I understand it.
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    #ForeignKey is the ID of each table how I understand it.
+    """ This line of code defines a foreign key relationship between the `Post` model and the `User` model in Django. A foreign key is a field in one table that refers to the primary key of another table. In this case, the `author` field in the `Post` model is a foreign key that refers to the `id` field in the `User` model. This means that each post is associated with a single user who created it.
+
+    The `on_delete` argument specifies what should happen when the referenced object (in this case, a `User` object) is deleted. `models.CASCADE` is a built-in Django option that specifies that when a `User` object is deleted, all of the `Post` objects associated with that `User` should also be deleted."""
 
     #agregamos metodo double underscore __str__ para customizar
     #el output de los posts:
--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from .models import Post
 
 def home(request):
     #en vez de lo de arriba, le pasamos la data de la clase Post guardada en la database:
-    context = {'posts': Post.objects.all()} 
+    context = {'posts': Post.objects.all(), 'titulo': 'Main Page'} 
     return render(request, 'blog/home.html', context)
    
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'Kasumi!'}) #argumentos siempre: request, template, context (diccionario)
+    return render(request, 'blog/about.html', {'titulo': 'Kasumi!'}) #argumentos siempre: request, template, context (diccionario)
